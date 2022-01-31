e980fe8ddc11dd69921cfead1a41a4b4b2db4d69
Epic91 <hernandeze791@gmail.com>
2022-01-31 12:39:59 -0600

    new challenge

---
+++ JS_Projects/CodeChallenges/sortSpeciesByTeeth.js
@@ -0,0 +1,14 @@
+/* 
+Write a function sortSpeciesByTeeth() that takes in an array of species objects in the format:
+
+{speciesName: 'shark', numTeeth: 50 }
+and sorts the array in ascending order based on the average number of teeth that species possesses (numTeeth) .
+
+You’ll need to access each object’s .numTeeth property. Feel free to either write a named comparison function, or use an anonymous function for your argument to .sort().
+*/
+
+const speciesArray = [ {speciesName:'shark', numTeeth:50}, {speciesName:'dog', numTeeth:42}, {speciesName:'alligator', numTeeth:80}, {speciesName:'human', numTeeth:32}];
+
+const sortSpeciesByTeeth = arr => arr.sort((speciesObj1, speciesObj2) => speciesObj1.numTeeth > speciesObj2.numTeeth)
+
+console.log(sortSpeciesByTeeth(speciesArray))
