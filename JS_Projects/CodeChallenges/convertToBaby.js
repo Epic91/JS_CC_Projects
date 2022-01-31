45afdb6af44205a300c35b7a9ae8031723234923
Epic91 <hernandeze791@gmail.com>
2022-01-31 12:23:05 -0600

    new challenge

---
+++ JS_Projects/CodeChallenges/convertToBaby.js
@@ -0,0 +1,12 @@
+//Write a function, convertToBaby(), that takes in an array as an argument and, using a loop, returns a new array with each string in the array prepended with 'baby '.
+
+const convertToBaby = arr => {
+    let babyArray = [];
+    for (let i = 0; i < arr.length; i++) {
+      babyArray.push(`baby ` + arr[i]);
+    }
+    return babyArray;
+  };
+  
+const animals = ["panda", "turtle", "giraffe", "hippo", "sloth", "human"];
+console.log(convertToBaby(animals));
